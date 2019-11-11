87b465a0ee324935989e5db9818bfcd21efb0eff
PedroHenriqueSouza <pedrosouzaferreira3@gmail.com>
2019-11-11 14:27:50 -0300

    22° Commit - Correção Pesquisa

--- model/tipo_acao/consultaNome.php
+++ model/tipo_acao/consultaNome.php
@@ -17,25 +17,27 @@ if(!$conn){
 // Recebe o valor enviado
 $valor = $_GET['valor'];
 
+
 // Procura titulos no banco relacionados ao valor
 $sql ="SELECT * FROM tipo_acao WHERE nome LIKE '%".$valor."%'";
 $consulta = mysqli_query($conn,$sql); 
 // Exibe todos os valores encontrados
-
+echo "
+<table class='table table-hover  mt-5 rounded'>
+    <thead>
+    <th scope='col' class='bg-dark text-light'>Nome</th>
+    <th width='30' scope='col' class='bg-dark text-light'></th>
+    <th width='30' scope='col' class='bg-dark text-light'></th>
+</tr>
+</thead>
+<tbody>
+";
 while ($noticias = mysqli_fetch_assoc($consulta)) {
 	
 	$id = $noticias['id'];
 	$nome = $noticias['nome'];
-
+    
 	echo  "       
-    <table class='table table-hover  mt-5 rounded'>
-    <thead>
-        <tr>
-            <th scope='col' class='bg-dark text-light'>Nome</th>
-            <th width='30' scope='col' class='bg-dark text-light'></th>
-            <th width='30' scope='col' class='bg-dark text-light'></th>
-        </tr>
-    </thead>
     <tbody>
     <tr>
      <td>$nome</td>   
@@ -43,7 +45,11 @@ while ($noticias = mysqli_fetch_assoc($consulta)) {
 
     ";
 }
- 
+    
+echo "
+</tbody>
+</table>
+";
 // Acentuação
 header("Content-Type: text/html; charset=ISO-8859-1",true);
 
